9329d85a03edcb844ffde998ee39ad92204e1f6c
anurag4dsb <anurag.mittal@scality.com>
2019-03-19 21:51:53 -0700

    node

---
+++ v3/notes-app/app.js
@@ -0,0 +1,12 @@
+const notes = require('./notes');
+
+notes();
+
+
+// const utils = require('./utils')
+
+// // const fs = require('fs')
+
+// // // fs.writeFileSync('notes.txt', 'My name is Anurag.');
+// // fs.appendFileSync('notes.txt', 'Appended text');
+// console.log(utils(1,2));
---
+++ v3/notes-app/notes.js
@@ -0,0 +1,5 @@
+const getNotes = function () {
+    console.log('getNotes');
+}
+
+module.exports = getNotes;
