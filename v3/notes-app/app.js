132e197095762640246038895228fa964fad5b09
anurag4dsb <anurag.mittal@scality.com>
2019-03-27 14:28:39 -0700

    completed personal notes app

--- v3/notes-app/app.js
+++ v3/notes-app/app.js
@@ -1,11 +1,90 @@
-// const fs = require('fs')
+const yargs = require('yargs');
 const notes = require('./notes')
 
+yargs.version('1.0.0-alpha');
 
-notes();
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Body of the note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        console.log('Adding note!', argv.title);
+        notes.addNote(argv.title, argv.body);
+    }
+})
+
+yargs.command({
+    command: 'delete',
+    describe: 'Delete a node',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        console.log('Deleting node', argv.title);
+        notes.deleteNote(argv.title);
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title);
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'update a note',
+    handler(argv) {
+        notes.listNotes();
+    }
+})
+
+yargs.parse();
 
 
-// const utils = require('./utils')
+
+
+
+
+
+
+// // const fs = require('fs')
+// // const validator = require('validator');
+// const chalk = require('chalk');
+// const notes = require('./notes');
+
+// notes();
+
+// console.log(chalk.bold.red.inverse('Error!!!'));
+
+// console.log(process.argv);
+// // console.log(validator.isURL('https:gmail.com'));
+
+// // const utils = require('./utils')
 
 // // const fs = require('fs')
 
--- v3/notes-app/notes.js
+++ v3/notes-app/notes.js
@@ -1,5 +1,71 @@
-const getNotes = () => {
-    console.log('your notes...');
+const fs = require('fs')
+const chalk = require('chalk')
+
+const addNote = (title, body) => {
+    const notes = loadNotes();
+    const duplicateNote = notes.find((note) => note.title === title)
+    console.log(duplicateNote);
+    if (!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes);
+        console.log(chalk.green.inverse.bold('Note added successfully!'));
+    } else {
+        console.log(chalk.red.inverse.bold('Duplicate note found! Either change note title or update.'));
+    }
 }
 
-module.exports = getNotes;
+const deleteNote = (title) => {
+    const notes = loadNotes();
+    const notesToKeep = notes.filter((note) => note.title !== title);
+    if (notes.length > notesToKeep.length) {
+        saveNotes(notesToKeep);
+        console.log(chalk.green.inverse.bold('Note removed!'));
+    }
+    else {
+        console.log(chalk.red.inverse.bold('Note not found!'));
+    }
+}
+
+const readNote = (title) => {
+    const notes = loadNotes();
+    note = notes.find(note => note.title === title);
+    if (note) {
+        console.log(chalk.inverse(note.title))
+        console.log(note.body)
+    } else {
+        console.log(chalk.red.inverse.bold('Note not found!'));
+    }
+}
+
+const listNotes = () => {
+    const notes = loadNotes();
+    console.log(chalk.inverse('Your notes!'));
+    notes.forEach((note) => {
+        console.log(note.title);
+    });
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataString = dataBuffer.toString();
+        return JSON.parse(dataString);
+    } catch(err) {
+        return []
+    }
+}
+
+const saveNotes = (notes) => {
+    const dataString = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataString);
+}
+
+module.exports = {
+    addNote,
+    deleteNote,
+    readNote,
+    listNotes
+}
